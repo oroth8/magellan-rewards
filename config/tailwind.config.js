a64d685dae9e1c4f6f082b5f65d8c4c1a7e6fd98
Owen Roth <rothowen27@gmail.com>
2023-12-26 14:19:36 -0600

    add landing page, with partials, setup tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,26 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        magellan: {
+          50: "#f5f7fa",
+          100: "#eaeef4",
+          200: "#d1dae6",
+          300: "#a8bbd1",
+          400: "#7997b7",
+          500: "#587a9f",
+          600: "#456184",
+          700: "#384e6c",
+          800: "#32435a",
+          900: "#2d3a4d",
+          950: "#252f3f",
+        },
+        primary: "#252f3f",
+        secondary: "#587a9f",
+        active: "#384e6c",
+        disabled: "#d1dae6",
+        text: "#384e6c",
+      },
     },
   },
   plugins: [
