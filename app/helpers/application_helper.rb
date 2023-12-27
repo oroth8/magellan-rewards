4bf647c368ce99fa767909ae87cc78d7524dfce7
Owen Roth <rothowen27@gmail.com>
2023-12-27 10:50:49 -0600

    add basic tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def set_meta_tags(title: nil, description: nil, keywords: nil)
+        content_for :title, title || 'Magellan Rewards'
+        content_for :meta_tags do
+          safe_join([
+            tag.meta(name: 'description', 
+content: description || 'An application for local rewards, discounts, and information for the Magellan communities.'),
+            tag.meta(name: 'keywords', content: keywords || 'Magellan Rewards')
+          ], "\n")
+        end
+      end
 end
