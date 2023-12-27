64f418f547940517bf922f23eb86830b59471243
Owen Roth <rothowen27@gmail.com>
2023-12-27 14:25:45 -0600

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_27_200309) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_202004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -26,6 +26,20 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_200309) do
     t.string 'name', default: '', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'partners', force: :cascade do |t|
+    t.string 'name', default: '', null: false
+    t.string 'address'
+    t.string 'city'
+    t.string 'state'
+    t.string 'postal_code'
+    t.boolean 'active', default: true, null: false
+    t.text 'description'
+    t.bigint 'category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_partners_on_category_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -53,4 +67,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_200309) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+  add_foreign_key 'partners', 'categories'
 end
