00dbf969cc163016429e2c56bc712fb718acd9a3
Owen Roth <rothowen27@gmail.com>
2023-12-27 20:09:42 -0600

    add image to partner model

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,4 +2,21 @@
 
 class Partner < ApplicationRecord
     has_one_attached :image
+
+    validate :acceptable_image
+
+    delegate :attached?, to: :image, prefix: true
+
+    private
+
+  def acceptable_image
+    return unless image.attached?
+
+    errors.add(:image, 'is too big') unless main_image.blob.byte_size <= 1.megabyte
+
+    acceptable_types = ['image/jpeg', 'image/png']
+    return if acceptable_types.include?(main_image.content_type)
+
+    errors.add(:image, 'must be a JPEG or PNG')
+  end
 end
